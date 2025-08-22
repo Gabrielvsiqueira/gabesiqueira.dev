426d0d9a1fbb094481e4c90a2d0d138b6eed3276
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-08-21 22:12:23 -0300

    feat: add project file into a project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
