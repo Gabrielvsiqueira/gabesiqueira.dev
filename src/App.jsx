e9f043528125322322b1335dfb64b81dd270553d
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-09-04 18:09:52 -0300

    feat: add project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects.jsx";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
