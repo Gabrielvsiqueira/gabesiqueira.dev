6d41543c47f17be4f324970e6a0aaf5f3de53071
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-09-04 21:21:52 -0300

    feat: add variations for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = ({ image, title, description, badgeText, tags }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-sm">
-      <figure>
-        <img src={image}/>
+      <figure className="h-48 w-full overflow-hidden flex items-center justify-center">
+        <img src={image} className="h-full w-full object-cover" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
@@ -12,7 +12,7 @@ const Card = ({ image, title, description, badgeText, tags }) => {
           {badgeText && <div className="badge badge-secondary">{badgeText}</div>}
         </h2>
         <p>{description}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-center flex-wrap mt-4 gap-2">
           {tags && tags.map((tag, index) => (
             <div key={index} className="badge badge-outline">{tag}</div>
           ))}
