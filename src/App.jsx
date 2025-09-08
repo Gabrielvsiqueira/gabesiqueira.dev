b7f7f8aeabba259c5f1e79d64fa1820f74b40775
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-09-08 17:36:03 -0300

    add route to honors page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects.jsx";
+import Honors from './pages/Honors.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/honors" element={<Honors />} />
       </Routes>
     </BrowserRouter>
   );
